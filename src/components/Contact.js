e2244a1bf3b1ba59977e99f5261ff5a721d3c524
Hunter Chang <hunter@madwiremedia.com>
2018-03-25 11:27:57 -0600

    new form tag

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,8 @@ const Contact = (props) => (
     <section id="contact">
         <div className="inner">
             <section>
-                <form method="post" action="#">
+                <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                    <input type="hidden" name="bot-field" />
                     <div className="field half first">
                         <label htmlFor="name">Name</label>
                         <input type="text" name="name" id="name" />
