57e07848d8cfc382861ef3e98fbc4059b2ca0f89
Hunter Chang <hunter@madwiremedia.com>
2018-03-25 14:05:43 -0600

    adding success page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ const Contact = (props) => (
     <section id="contact">
         <div className="inner">
             <section>
-                <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                <form name="contact" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
                     <input type="hidden" name="bot-field" />
                     <div className="field half first">
                         <label htmlFor="name">Name</label>
