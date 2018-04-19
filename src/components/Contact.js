86fa29f7dec6cd2debcdfc4b160ab09348b61781
Hunter Chang <hunter@madwiremedia.com>
2018-04-18 21:57:27 -0600

    add required

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,15 +8,15 @@ const Contact = (props) => (
                     <input type="hidden" name="bot-field" />
                     <div className="field half first">
                         <label htmlFor="name">Name</label>
-                        <input type="text" name="name" id="name" />
+                        <input type="text" name="name" id="name" required />
                     </div>
                     <div className="field half">
                         <label htmlFor="email">Email</label>
-                        <input type="text" name="email" id="email" />
+                        <input type="text" name="email" id="email" required />
                     </div>
                     <div className="field">
                         <label htmlFor="message">Message</label>
-                        <textarea name="message" id="message" rows="6"></textarea>
+                        <textarea name="message" id="message" rows="6" required></textarea>
                     </div>
                     <ul className="actions">
                         <li><input type="submit" value="Send Message" className="special" /></li>
